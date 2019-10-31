806b8e81afbe9b301e8d87116486ee65bb0cbfe0
codenameAngelikaTeam <albertroadriguez@hotmail.co.id>
2019-10-31 14:30:40 +0000

    main data

---
+++ thank.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+	<?php
+	header("Refresh:5; url=index.html");
+	?>
+<center style="font-size: 13pt">
+Thank you for your time,  <?php echo $_POST["Username"]; ?><br>
+Have a nice day !
+</center>
+</body>
+</html>
